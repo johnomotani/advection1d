6d320cda0fd6c37579b1e51241ebb33992070546
John Omotani <john.omotani@ukaea.uk>
2020-07-12 17:25:59 +0100

    Parameters struct

--- main.cxx
+++ main.cxx
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include "parameters.hxx"
+
 int main() {
 
+  const Parameters parameters;
+
   std::cout<<"running 1d advection..."<<std::endl;
+
+  std::cout<<std::endl<<"Parameters"<<std::endl<<"----------"<<std::endl;
+  std::cout<<"Nz\t"<<parameters.Nz<<std::endl;
+  std::cout<<"L\t"<<parameters.L<<std::endl;
+  std::cout<<"dt\t"<<parameters.dt<<std::endl;
+  std::cout<<"t_out\t"<<parameters.t_out<<std::endl;
+  std::cout<<"N_out\t"<<parameters.N_out<<std::endl;
 
   return 0;
 }
---
+++ parameters.hxx
@@ -0,0 +1,16 @@
+struct Parameters {
+  /// Number of spatial grid points
+  int Nz = 32;
+
+  /// Size of spatial domain
+  double L = 1.0;
+
+  /// internal time-step
+  double dt = .01;
+
+  /// time between outputs
+  double t_out = 1.0;
+
+  /// number of outputs
+  int N_out = 100;
+};
