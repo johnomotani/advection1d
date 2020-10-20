054071c5b77bd3986e7d839db8c3a03b93afe5b0
John Omotani <john.omotani@ukaea.uk>
2020-10-20 18:57:09 +0100

    Multiprocessing option for scan

--- runs/performance_scan.py
+++ runs/performance_scan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import h5py
+import multiprocessing
 from pathlib import Path
 from statistics import mean
 import subprocess
@@ -9,9 +10,21 @@ import toml
 from plot_performance_scan import plot_scan
 
 n_values = list(range(8, 129)) + [256]
+models = [
+    "chebyshevmatrix",
+    "chebyshevfft",
+    "chebyshevfft_r2r",
+    "chebyshevfft_r2c",
+]
+
+inputs = [(model, n) for model in models for n in n_values]
 
 base_directory = Path().absolute()
 advection1d_path = Path(__file__).parent.joinpath("..", "advection1d").absolute()
+
+# Use half of cpus to ensure system is not full - attempt to make sure timing is not
+# affected by time used for system processes, etc.
+n_procs = multiprocessing.cpu_count() // 2
 
 with open("input.toml") as f:
     base_options = toml.load(f)
@@ -28,12 +41,16 @@ def run_sim(run_dir):
     return float(stdout_lines[-2].split()[-1][:-1])
 
 
-def time_case(model, n):
+def time_case(inputs):
+    model, n = inputs
     print(model, n, flush=True)
 
     # set up input
     p = Path(f"{model}_N{n}")
-    p.mkdir()
+    try:
+        p.mkdir()
+    except FileExistsError:
+        return model, n, None
     options = base_options.copy()
     options["spatial_type"] = model
     options["N"] = n
@@ -41,27 +58,21 @@ def time_case(model, n):
         toml.dump(options, f)
 
     # repeat runs to make timing more reliable
-    return [run_sim(p) for i in range(5)]
+    return model, n, [run_sim(p) for i in range(5)]
 
 
 results_file = h5py.File("scan.h5", "a")
-for model in [
-    "chebyshevmatrix",
-    "chebyshevfft",
-    "chebyshevfft_r2r",
-    "chebyshevfft_r2c",
-]:
-    try:
-        model_group = results_file[model]
-    except KeyError:
-        model_group = results_file.create_group(model)
+for model in models:
+    if model not in results_file:
+        results_file.create_group(model)
 
-    for n in n_values:
-        try:
-            times = time_case(model, n)
-        except FileExistsError:
+with multiprocessing.Pool(n_procs) as pool:
+    for model, n, times in pool.imap_unordered(time_case, inputs):
+        if times is None:
             # case has already been run, so skip
             continue
+
+        model_group = results_file[model]
 
         if str(n) in model_group:
             # directory was deleted, so re-running for some reason.
